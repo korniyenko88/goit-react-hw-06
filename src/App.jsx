9766f1e18619562d73e28aa74f4f5f4f622995af
unknown <you@example.com>
2024-10-29 16:28:20 +0200

    add and del contacts

--- src/App.jsx
+++ src/App.jsx
@@ -5,38 +5,46 @@ import SearchBox from './components/SearchBox/SearchBox';
 import ContactForm from './components/ContactForm/ContactForm';
 import { nanoid } from 'nanoid';
 import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 function App() {
-  const [contacts, setContacts] = useState(() => {
-    const storedContacts = localStorage.getItem('contacts');
-    return storedContacts
-      ? JSON.parse(storedContacts)
-      : [
-          { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-          { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-          { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-          { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-        ];
-  });
+  // const [contacts, setContacts] = useState(() => {
+  //   const storedContacts = localStorage.getItem('contacts');
+  //   return storedContacts
+  //     ? JSON.parse(storedContacts)
+  //     : [
+  //         { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  //         { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  //         { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  //         { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  //       ];
+  // });
+
+  const contacts = useSelector(state => state.contactsData.contacts);
 
   useEffect(() => {
     localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
-
+  const dispatch = useDispatch();
   const [searchTerm, setSearchTerm] = useState('');
+  
   const onAddContact = formData => {
     const finalContact = {
       ...formData,
       id: nanoid(),
     };
-    setContacts(prev => [...prev, finalContact]);
-  };
+  
+    const action = { type: 'contacts/addContacts', payload: finalContact };
+    dispatch(action);
+  }
+
+    
+  //   setContacts(prev => [...prev, finalContact]);
+  // };
 
   const onDeleteContact = contactId => {
-    const updatedContacts = contacts.filter(
-      contact => contact.id !== contactId
-    );
-    setContacts(updatedContacts);
+   const action = { type: 'contacts/delContacts', payload: contactId };
+   dispatch(action);
   };
 
   const handleSearchChange = event => {
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { contactsReducer } from "./contacts/contactsReducer";
 
 
 export const store = configureStore({
-    
+    reducer: {
+        contactsData: contactsReducer
+    }
 })
+
